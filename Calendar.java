ec717b8584d2e96f48360a900f81e6d05eb7de8e
sauv1gnon <brent_rebollos@dlsu.edu.ph>
2025-06-03 00:23:21 +0800

    added calendar hearder display incomplete: true

--- Calendar.java
+++ Calendar.java
@@ -14,6 +14,20 @@ public class Calendar {
         this.entries = new ArrayList<>();
     }
 
+    // display top calendar information
+    /*
+     * ##########################
+     * #\t Calendar Name: abacadabra\t #
+     * # Sunday # Monday # Tuesday # Wednesday # Thursday # Friday # Saturday #
+     * ##########################
+     */
+    public void displayCalendarDetails() {
+        System.out.println("##########################");
+        System.out.printf("#\t Calendar Name: %s\t #\n", this.name);
+        System.out.println("# Sunday # Monday # Tuesday # Wednesday # Thursday # Friday # Saturday #");
+        System.out.println("##########################");
+    }
+
     public String getName() {
         return name;
     }
