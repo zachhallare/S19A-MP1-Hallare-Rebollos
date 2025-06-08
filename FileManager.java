8113af211ca69cff7dea1bdb4167356732f1f726
Zach Benedict Hallare <zachhallare@gmail.com>
2025-06-09 07:47:11 +0800

    File writing method now completed.

--- FileManager.java
+++ FileManager.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -14,8 +16,8 @@ public class FileManager {
             String lineInFile;
             while ((lineInFile = reader.readLine()) != null) {
                 String[] userData = lineInFile.split(",");
-                String username = userData[0];
-                String password = userData[1];
+                String username = userData[0].trim();
+                String password = userData[1].trim();
                 accounts.add(new Account(username, password));
             }
         }
@@ -24,5 +26,16 @@ public class FileManager {
         }
 
         return accounts;
+    }
+
+    // Add a new account to the file.
+    public static void addAccount(Account account) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {     // true = it will appead in the file.
+            writer.write(account.getUsername() + ", " + account.getPassword());
+            writer.newLine();
+        }  
+        catch (IOException error) {
+            System.out.println("File writing error: " + error.getMessage());
+        }     
     }
 }
