5c4166f5c252044e56dd6e54719cdc7642630caf
Zach Benedict Hallare <zachhallare@gmail.com>
2025-06-22 09:36:40 +0800

    Tested and compiled.

--- s19a_mp1_hallare_rebollos/src/main/java/com/hallareandrebollos/Main.java
+++ s19a_mp1_hallare_rebollos/src/main/java/com/hallareandrebollos/Main.java
@@ -4,17 +4,18 @@ import com.hallareandrebollos.lib.TextInterface;
 
 public class Main {
     public static void main(String[] args) {
-        // im just testing it, but we can make this as the main thing too.
-
         TextInterface ui = new TextInterface();
 
         while (ui.getPageIndex() != -1) {
             switch (ui.getPageIndex()) {
+                // Main Page.
                 case 1 -> {
                     ui.MenuPage();
                     ui.MenuPageLogic();
                 }
+                // Display Calendar.
                 case 2 -> ui.CalendarDisplayController();
+                // Login Page.
                 default -> {
                     ui.LoginPage();
                     ui.LoginPageLogic();
