8262e7b11f59f369f73dec8cbaea626efca3cf7a
Zach Benedict Hallare <zachhallare@gmail.com>
2025-06-24 18:10:26 +0800

    .

--- s19a_mp1_hallare_rebollos/src/main/java/com/hallareandrebollos/Main.java
+++ s19a_mp1_hallare_rebollos/src/main/java/com/hallareandrebollos/Main.java
@@ -8,24 +8,28 @@ public class Main {
 
         while (ui.getPageIndex() != -1) {
             switch (ui.getPageIndex()) {
-                case 1 -> {
-                    // Main Page.
-                    ui.menuPage();
-                    ui.loadCalendarList();
-                    ui.menuPageLogic();
-                }
-                case 2 -> {
-                    // Display Calendar.
-                    ui.loadCalendarList();
-                    ui.calendarDisplayController();
-                }
-                default -> {
+                case 0 -> {
                     // Login Page.
                     ui.loginPage();
                     ui.loginPageLogic();
                 }
+                case 1 -> {
+                    // Main Page.
+                    ui.menuPage();
+                    ui.menuPageLogic();
+                }
+                case 2 -> {
+                    // Display Calendar.
+                    ui.calendarMenuPage();
+                    ui.calendarPageLogic(0);
+                }
+                default -> {
+                    System.out.println("Unknown page index. Returning to login page...\n\n");
+                    ui.setPageIndex(0);
+                }
             }
         }
 
+        System.out.println("Program exited. Goodbye!");
     }
 }
