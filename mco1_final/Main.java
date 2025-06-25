98c4e967caea3399e1dac1f5a81ae9b1ff95fcb9
Zach Benedict Hallare <zachhallare@gmail.com>
2025-06-26 07:05:41 +0800

    Updated the Main.java

--- mco1_final/Main.java
+++ mco1_final/Main.java
@@ -2,29 +2,27 @@ public class Main {
     public static void main(String[] args) {
         LogicController logicController = new LogicController();
         DisplayController displayController = new DisplayController(logicController);
-        String currentPage = "landing"; // Start at the login page
+        String currentPage = "landing";     // Start at the login page
+        boolean isRunning = true;           // Loop control variable
 
-        while (currentPage != null || currentPage != "exit") {
+        while (isRunning) {
             switch (currentPage) {
-                case "landing":
-                    currentPage = displayController.displayLandingPage();
-                    break;
-                case "login":
-                    currentPage = displayController.displayLoginPage();
-                    break;
-                case "signup":
-                    currentPage = displayController.displaySignUpPage();
-                    break;
-                case "menu":
-                    currentPage = displayController.displayMenuPage();
-                    break;
-                case "calendar":
-                    
-                    break;
-                default:
+                case "landing" -> currentPage = displayController.displayLandingPage();
+                case "login" -> currentPage = displayController.displayLoginPage();
+                case "signup" -> currentPage = displayController.displaySignUpPage();
+                case "menu" -> currentPage = displayController.displayMenuPage();
+                case "calendar" -> {
+                    displayController.displayMonthSelection();
+                    currentPage = "menu";
+                }
+                case "exit" -> {
+                    System.out.println("Thank you for using our Digital Calendar. Goodbye!");
+                    isRunning = false; 
+                }
+                default -> {
                     System.out.println("Unknown page. Returning to portal page...");
                     currentPage = "landing";
-                    break;
+                }
             }
         }
     }
